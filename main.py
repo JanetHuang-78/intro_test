d5bae9173b95225b37478cc4a80c047de41bc860
Janet Huang <62869320+JanetHuang-78@users.noreply.github.com>
2021-11-28 22:45:12 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from HelloWorld import say_hello_world
+from my_other_dir.greetings import say_hi
+
+def main():
+    res1 = say_hello_world()
+    res2 = say_hi('Annie')
+    return res1 + '' +res2
+
+if __name__ == '__main__':
+    print(main())
